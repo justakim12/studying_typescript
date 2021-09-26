feddf51d7f94a659a4baff09380950c003b90eaf
David K. Kim <rlaruddlf711@naver.com>
2021-09-26 23:48:49 +0900

    Add samples of typescript

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = 'Traversy Media';
+let isPublised = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Brad', true];
+//Tuple Array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill']
+];
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// Basic Types
+let id: number = 5
+let company: string = 'Traversy Media'
+let isPublised: boolean =  true
+let x: any = 'Hello'
+
+let ids: number[] = [1,2,3,4,5]
+let arr: any[] = [1, true, 'Hello']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Brad', true]
+
+//Tuple Array
+let employee: [number, string][]
+
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill']
+]
