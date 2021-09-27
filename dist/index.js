6ef58508e6afcda8e15f7580557e5eb56e64c401
David K. Kim <rlaruddlf711@naver.com>
2021-09-27 10:45:23 +0900

    Update typescript examples

--- dist/index.js
+++ dist/index.js
@@ -15,3 +15,68 @@ employee = [
     [2, 'John'],
     [3, 'Jill']
 ];
+//Union
+let pid;
+pid = '22';
+//Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Left);
+const user = {
+    id: 1,
+    name: 'John'
+};
+//Type Assertion
+let cid = 1;
+let customerId = cid;
+//let customerId = <number>cid
+//Functions
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(1, 2));
+//Void type
+function log(message) {
+    console.log(message);
+}
+log(4);
+const user1 = {
+    id: 1,
+    name: 'John'
+};
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+//Classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const brad = new Person(1, 'Brad Traversy');
+const mike = new Person(1, 'Mike Kim');
+console.log(brad, mike);
+console.log(brad.register());
+//Subclass using extends
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name);
+        this.position = position;
+    }
+}
+const emp = new Employee(3, 'Shawn', 'Developer');
+console.log(emp.register());
+//Generics
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArray = getArray([1, 2, 3, 4]);
+let stringArray = getArray(['brad', 'John', 'Jill']);
--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,112 @@ employee = [
     [2, 'John'],
     [3, 'Jill']
 ]
+
+//Union
+let pid: string | number 
+pid = '22'
+
+//Enum
+enum Direction1 {
+    Up = 1,
+    Down,
+    Left,
+    Right
+}
+
+console.log(Direction1.Left)
+
+//Objects
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: 'John'
+}
+
+//Type Assertion
+let cid: any = 1
+let customerId = cid as number 
+//let customerId = <number>cid
+
+//Functions
+function addNum(x: number, y: number): number {
+    return x + y
+}
+
+console.log(addNum(1,2))
+
+//Void type
+function log(message: string | number): void {
+    console.log(message)
+}
+
+log(4)
+
+//Interfaces
+interface UserInterface {
+    readonly id: number,
+    // read only. can't write
+    name: string
+    age?: number
+    // optional
+}
+
+const user1: UserInterface = {
+    id: 1,
+    name: 'John'
+}
+
+interface MathFunc {
+    (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
+
+//Classes
+class Person {
+    id: number
+    name: string
+
+    constructor(id: number, name: string) {
+        this.id = id
+        this.name = name
+    }
+
+    register() {
+        return `${this.name} is now registered`
+    }
+}
+
+const brad = new Person(1, 'Brad Traversy')
+const mike = new Person(1, 'Mike Kim')
+
+console.log(brad, mike)
+
+console.log(brad.register())
+
+//Subclass using extends
+class Employee extends Person {
+    position: string
+
+    constructor(id: number, name: string, position: string) {
+        super(id, name)
+        this.position = position
+    }
+}
+
+const emp = new Employee(3, 'Shawn', 'Developer')
+
+console.log(emp.register())
+
+//Generics
+function getArray<T>(items: T[]): T[] {
+    return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1,2,3,4])
+let stringArray = getArray<string>(['brad', 'John', 'Jill'])
